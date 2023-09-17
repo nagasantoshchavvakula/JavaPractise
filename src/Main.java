3e03c1245016ffedd73becda0f9160d883d8dc00
Chandrashekar <nagasantoshchavvakula@gmail.com>
2023-09-16 20:42:44 -0500

    Multiple Classes with methods

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main{
+    public void fullThrottle(){
+        System.out.println("Full Thnrottle method");
+    }
+    public void Speed(int maxSpeed){
+        System.out.println("Max Speed is"+maxSpeed);
+    }
+}
---
+++ src/Second.java
@@ -0,0 +1,7 @@
+public Class Second{
+    public static void Main(String[]args){
+    Main myCar=new Main();
+    myCar.fullThrottle();
+    myCar.Speed(200);
+}
+}
